f5bfc298c558bc174c69ceede08b1ecef9eafcf5
Cristiano <cristiano.tarconi@gmail.com>
2021-01-08 23:45:24 +0100

    prev + next arrows running

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
 
+    //*Attivazione freccia scorrimento avanti (dx)
     $('.next i').click(function(){
         var selectedImage = $('.img_container img.active');
         selectedImage.removeClass('active');
-        selectedImage.next().addClass('active');
+
+        if (selectedImage.hasClass('last')) {
+            $('.img_container img.first').addClass('active')
+        } else {
+            selectedImage.next().addClass('active');
+        }
+    });
+
+    //*Attivazione freccia scorrimento indietro (sx)
+    $('.prev i').click(function(){
+        var selectedImage = $('.img_container img.active');
+        selectedImage.removeClass('active');
+
+        if (selectedImage.hasClass('first')) {
+            $('.img_container img.last').addClass('active')
+        } else {
+            selectedImage.prev().addClass('active');
+        }
     });
 
 });
