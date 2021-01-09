76ca9fe3047902ff6e9241618955e95aedc369f0
Cristiano <cristiano.tarconi@gmail.com>
2021-01-10 00:37:01 +0100

    add cars logo + jquery next/prev running

--- js/script.js
+++ js/script.js
@@ -8,12 +8,17 @@ $(document).ready(function(){
         var selectedBar = $('.selection_bar .red_bar.active');
         selectedBar.removeClass('active');
 
+        var selectedLogo = $('.brand_box img.active');
+        selectedLogo.removeClass('active');
+
         if (selectedImage.hasClass('last')) {
             $('.img_container img.first').addClass('active');
             $('.selection_bar .red_bar.first').addClass('active');
+            $('.brand_box img.first').addClass('active');
         } else {
             selectedImage.next().addClass('active');
             selectedBar.next().addClass('active');
+            selectedLogo.next().addClass('active');
         }
     });
 
@@ -25,12 +30,17 @@ $(document).ready(function(){
         var selectedBar = $('.selection_bar .red_bar.active');
         selectedBar.removeClass('active');
 
+        var selectedLogo = $('.brand_box img.active');
+        selectedLogo.removeClass('active');
+
         if (selectedImage.hasClass('first')) {
             $('.img_container img.last').addClass('active');
             $('.selection_bar .red_bar.last').addClass('active');
+            $('.brand_box img.last').addClass('active');
         } else {
             selectedImage.prev().addClass('active');
             selectedBar.prev().addClass('active');
+            selectedLogo.prev().addClass('active');
         }
     });
 
@@ -47,6 +57,10 @@ $(document).ready(function(){
     
         $('.img_container img.active').removeClass('active');
         $('.img_container').children().eq(index).addClass('active');
+        
+        $('.brand_box img.active').removeClass('active');
+        $('.brand_box').children().eq(index).addClass('active');
+
     });
 
 });
